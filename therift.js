c56d9f131758aba54454b809136a2a7f4bdd1220
charlonyx <charlon@bu.edu>
2015-11-15 10:10:03 -0500

    settings button
    added settings button and reset button

--- therift.js
+++ therift.js
@@ -3,39 +3,42 @@ WRITTEN BY: Rosanne Hu & Remi Kobayashi
 DATE: November 13, 2015
 PURPOSE: JS file for the rift*/
 
-var setting = 'off';
-var width = 5
-
+var width = 5;
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
 function onOff(){
-    if(setting == on){
-        var setting = 'off';
-        $('')
-    }else if(setting == 'off'){
-        var setting ='on';
-    }
+    $(".settings-button").toggleClass('hidden');
+    /*if(hasClass(document.getElementById('settings'), "off")){
+        document.getElementById('settings').className = "button";
+        document.getElementById('settings-buttons').css.style.display = hide;
+    }else {
+        document.getElementById('settings').className = "button off";
+        document.getElementById('settings-buttons').css.style.display = show;
+    }*/
 }
 
-function hover(x,y){
+/*function hover(x,y){
     if((x>0 && x<25) && (y>0 && x<15){
         $('button').click(onOff);
     }
     if((timeUp-timedown)>3)
         click
     }
-}
+}*/
 
 function getWidth(){
     return width
 }
 
 
-function settings(setting){
+/*function settings(setting){
     //
     if(position==){
         $('button').click(settings);
     }
 
-}
+}*/
 
 function Reset(){
     //var Hardness = clay
@@ -48,16 +51,16 @@ function Reset(){
 //down
 //left
 //right
-$('button').click(rh)
+//$('button').click(rh)
 //undo
-$('button').click(ph)
+//$('button').click(ph)
 //changeMaterial
-$()
-$('button').click()
-$('reset').click(Reset)
-$('button').click(settings);
+//$()
+//$('button').click()
+//$('reset').click(Reset)
+//$('button').click(settings);
 
 
-function randomElt(array) {
+/*function randomElt(array) {
     return array[Math.floor(array.length*Math.random())];
-}
+}*/
