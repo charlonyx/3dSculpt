4edc6f0294094c5dc31b6ee3c519bd37f601cff6
starrynight56 <rhu2@wellesley.edu>
2015-11-15 06:47:43 -0500

    settings and reset function
    sadad

---
+++ therift.js
@@ -0,0 +1,63 @@
+/*FILE NAME: therift.js
+WRITTEN BY: Rosanne Hu & Remi Kobayashi
+DATE: November 13, 2015
+PURPOSE: JS file for the rift*/
+
+var setting = 'off';
+var width = 5
+
+function onOff(){
+    if(setting == on){
+        var setting = 'off';
+        $('')
+    }else if(setting == 'off'){
+        var setting ='on';
+    }
+}
+
+function hover(x,y){
+    if((x>0 && x<25) && (y>0 && x<15){
+        $('button').click(onOff);
+    }
+    if((timeUp-timedown)>3)
+        click
+    }
+}
+
+function getWidth(){
+    return width
+}
+
+
+function settings(setting){
+    //
+    if(position==){
+        $('button').click(settings);
+    }
+
+}
+
+function Reset(){
+    //var Hardness = clay
+    var Motion = shape
+}
+
+
+
+//up
+//down
+//left
+//right
+$('button').click(rh)
+//undo
+$('button').click(ph)
+//changeMaterial
+$()
+$('button').click()
+$('reset').click(Reset)
+$('button').click(settings);
+
+
+function randomElt(array) {
+    return array[Math.floor(array.length*Math.random())];
+}
